945553d80a75985af3cf562601098dac175fb053
arnxxau <arnxxau@gmail.com>
2020-09-12 17:20:33 +0200

    test

--- app/src/main/java/com/example/kotsic/MainActivity.kt
+++ app/src/main/java/com/example/kotsic/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val test = "hello"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
 
-fun test() {}
+fun test() {
+
+}
