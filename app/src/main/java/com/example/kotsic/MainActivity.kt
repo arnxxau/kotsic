0a733aed7784be7a737fcc53dbfcd8b950f4a5c2
arnxxau <arnxxau@gmail.com>
2020-09-13 04:35:58 +0200

    test

--- app/src/main/java/com/example/kotsic/MainActivity.kt
+++ app/src/main/java/com/example/kotsic/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     val test = "hellyo"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_play_song)
     }
 }
 
